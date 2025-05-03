57bdf4b39a78883692cda27be93824bf47adde17
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-03 19:50:48 +0400

    Выполнила задание 6

---
+++ CSharpEducation.Practice/Practice1.Task6/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double cA = 6.5;
+        double cB = 15;
+        double c = Math.Sqrt(cA * cA + cB * cB);
+        Console.WriteLine("Исходные данные:");
+        Console.WriteLine("Катет a = " + cA);
+        Console.WriteLine("Катет b = " + cB);
+        Console.WriteLine("Гипотенуза c = " + c);
+    }
+}
