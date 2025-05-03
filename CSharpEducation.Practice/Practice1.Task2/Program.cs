d8e9ce5dd2f263c37ca9d20fa2fc99689631b369
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-03 19:46:22 +0400

    Выполнила задание 2

---
+++ CSharpEducation.Practice/Practice1.Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age = 23;
+        string name = "Настя";
+        double temp = 25.3; 
+        bool gender = true;
+        Console.WriteLine("Женский пол");
+    }
+}
