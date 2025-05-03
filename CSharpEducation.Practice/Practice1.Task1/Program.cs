e7b8fea6dea2349c4f16d16eb1eff2307a176afd
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-03 19:44:50 +0400

    Добавила 1 задание

---
+++ CSharpEducation.Practice/Practice1.Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age;
+        string name;
+        string company;
+        bool logic;
+        float weight; 
+    }
+}
