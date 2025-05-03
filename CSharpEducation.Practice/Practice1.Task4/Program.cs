24ad069514c4ab7dd4672cb1af177e865c195efc
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-03 19:48:56 +0400

    Выполнила задание 5

---
+++ CSharpEducation.Practice/Practice1.Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Привет, напиши мне! ;)");
+        string a = Console.ReadLine();
+        string b = Console.ReadLine();
+        Console.WriteLine(a + " " + b);
+    }
+}
