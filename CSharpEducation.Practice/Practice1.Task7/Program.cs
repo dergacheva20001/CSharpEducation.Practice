3ef173d73c4320d68534e469fd7a7e9c423cfc51
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-03 19:51:44 +0400

    Выполнила задание 7

---
+++ CSharpEducation.Practice/Practice1.Task7/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double cA = 6.3;
+        double c = 17;
+        double cB = Math.Sqrt(c * c - cA * cA);
+        Console.WriteLine("Исходные данные:");
+        Console.WriteLine("Катет a = " + cA);
+        Console.WriteLine("Гипотенуза с = " + c);
+        Console.WriteLine("Катет b = " + cB);
+    }
+}
