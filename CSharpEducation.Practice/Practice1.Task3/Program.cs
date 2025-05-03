3b6eb7b96784421b4b91d8d62214b5aa5200d98e
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-03 19:47:49 +0400

    Выполнила задание 3

---
+++ CSharpEducation.Practice/Practice1.Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Привет, как тебя зовут?");
+        string name = Console.ReadLine();
+        Console.WriteLine("Привет, " + name + "!");
+    }
+}
