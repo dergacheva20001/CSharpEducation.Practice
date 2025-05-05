759e636b793b22c113843dfe5afaba614d4b5385
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-05 18:50:54 +0400

    пр2 задание 6b

---
+++ CSharpEducation.Practice/Practice2.Task6b/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите строку:");
+string inputString = Console.ReadLine();
+
+Console.WriteLine("Введите символ для поиска:");
+char targetChar = Console.ReadKey().KeyChar; 
+Console.WriteLine(); 
+
+if (string.IsNullOrEmpty(inputString))
+{
+    Console.WriteLine("Строка не должна быть пустой.");
+    return;
+}
+
+double charCount = 0;
+foreach (char c in inputString)
+{
+    if (char.ToLower(c) == char.ToLower(targetChar)) 
+    {
+        charCount++;
+    }
+}
+
+double percentage = (charCount / inputString.Length) * 100;
+
+Console.WriteLine($"Процент вхождения символа '{targetChar}' в строку: {percentage:F2}%");
