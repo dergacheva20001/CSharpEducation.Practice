37ba73ca6c6cc1d71387fed3f6592ec9e84e4176
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-05 18:07:51 +0400

    пр2 задание 2b

---
+++ CSharpEducation.Practice/Practice2.Task2b/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Введите количество километров: ");
+string input = Console.ReadLine();
+        
+if (int.TryParse(input, out int kilometers))
+{
+    int santimeters = kilometers * 100000;
+    
+    Console.WriteLine($"Сантиметры: {santimeters}");
+}
