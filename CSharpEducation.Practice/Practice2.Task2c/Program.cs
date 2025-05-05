730161df136480176e6622636766d238a4a2b449
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-05 18:14:31 +0400

    пр 2 задание 2с

---
+++ CSharpEducation.Practice/Practice2.Task2c/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Введите величину скорости км/час: ");
+string skorost = Console.ReadLine();
+
+if (double.TryParse(skorost, out double kilometerivchas))
+{
+    double metrivsekundu = kilometerivchas * 0.277778;
+            
+    Console.WriteLine($"{metrivsekundu}(м/с)");
+}
