e2bab4004efa8a55afbf3683580d4d4021e602d2
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-05 19:57:03 +0400

    пр2 задание 9

---
+++ CSharpEducation.Practice/Practice2.Task9/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+
+    static int[] CreateArray(int size)
+    {
+
+        int[] array = new int[size];
+
+
+        Random random = new Random();
+
+ 
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(1, 100); 
+        }
+
+        return array;
+    }
+
+    static void PrintArray(int[] array)
+    {
+
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine("Массив пустой или не существует.");
+            return;
+        }
+
+        Console.WriteLine("Элементы массива:");
+        foreach (int number in array)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine(); 
+    }
+
+    static void Main(string[] args)
+    {
+
+        int size = 10; 
+        int[] myArray = CreateArray(size); 
+        PrintArray(myArray); 
+    }
+}
