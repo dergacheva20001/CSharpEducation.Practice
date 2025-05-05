40c506ee48f45ce560695675a0e500f2bd66ef8b
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-05 14:53:33 +0400

    пр 2 задание 1d

---
+++ CSharpEducation.Practice/Practice2.Task1d/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Сколько слов вы хотите ввести?");
+int num = int.Parse(Console.ReadLine());
+
+string n = "";
+
+Console.WriteLine("Введите слова:");
+for (int i = 0; i < num; i++)
+{
+    Console.Write($"Слово {i + 1}: ");
+    string word = Console.ReadLine();
+    
+    if (i == 0)
+    {
+        n += word; 
+    }
+    else
+    {
+        n += " " + word; 
+    }
+}
+
+Console.WriteLine("Ваша фраза: " + n);
