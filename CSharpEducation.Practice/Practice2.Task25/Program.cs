21f80bf4e3a53d9d9fabed2b7d635704f1730098
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-05 20:53:35 +0400

    пр2 задание 25

---
+++ CSharpEducation.Practice/Practice2.Task25/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DaysOfWeekApp
+{
+    enum DayOfWeek
+    {
+        Monday = 1,   
+        Tuesday = 2,    
+        Wednesday = 3,   
+        Thursday = 4,    
+        Friday = 5,      
+        Saturday = 6,    
+        Sunday = 7       
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число от 1 до 7:");
+
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int dayNumber))
+            {
+                if (dayNumber >= 1 && dayNumber <= 7)
+                {
+
+                    DayOfWeek day = (DayOfWeek)dayNumber;
+
+                    Console.WriteLine($"День недели: {day}");
+                }
+                else
+                {
+                    Console.WriteLine("Число должно быть от 1 до 7.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Пожалуйста, введите корректное число.");
+            }
+        }
+    }
+}
