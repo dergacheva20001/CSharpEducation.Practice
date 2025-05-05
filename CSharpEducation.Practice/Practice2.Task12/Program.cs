37bcdd4c7d7d7eb39f6c10908d1b8c75458dfad7
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-05 20:03:04 +0400

    пр2 задание 12

---
+++ CSharpEducation.Practice/Practice2.Task12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int size = 5;
+        
+        string[] array = new string[size];
+        
+        for (int i = 0; i < size; i++)
+        {
+ 
+            Console.Write($"Введите строку {i + 1}: ");
+            
+            array[i] = Console.ReadLine();
+        }
+        
+        Console.WriteLine("\nВы ввели следующие строки:");
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
+    }
+}
