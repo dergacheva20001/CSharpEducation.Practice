5042b09f0dd4cbb7fb8b32e2dc282b91ff11de7b
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-06 13:32:59 +0400

    Добавила задание 6A

---
+++ CSharpEducation.Practice/Practice2.Task6a/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+{
+    string inputString = "Пример строки для подсчета символов";
+    char targetChar = 'о';
+    
+    int count = 0;
+    foreach (char c in inputString)
+    {
+        if (c == targetChar)
+        {
+            count++;
+        }
+    }
+
+    double percentage = (double)count / inputString.Length * 100;
+    
+    Console.WriteLine($"Символ '{targetChar}' встречается в строке {count} раз.");
+    Console.WriteLine($"Процент вхождения символа '{targetChar}' в строку: {percentage:F2}%");
+}
