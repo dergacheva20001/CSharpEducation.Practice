a8255b3f4286823625227e799025d7de8e791bc4
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-06 13:38:13 +0400

    Добавила задание 22B

---
+++ CSharpEducation.Practice/Practice2.Task22b/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class GuessTheNumber
+{
+    public static void Main(string[] args)
+    {
+        Random random = new Random();
+        int secretNumber = random.Next(1, 101);
+        int guess = 0;
+        int attempts = 0;
+
+        Console.WriteLine("Добро пожаловать в игру 'Угадай число'!");
+        Console.WriteLine("Я загадал число от 1 до 100.");
+
+        while (guess != secretNumber)
+        {
+            attempts++;
+
+            Console.Write($"Попытка {attempts}: Введите ваше предположение: ");
+
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out guess))
+            {
+                if (guess > secretNumber)
+                {
+                    Console.WriteLine("Загаданное число меньше.");
+                }
+                else if (guess < secretNumber)
+                {
+                    Console.WriteLine("Загаданное число больше.");
+                }
+                else
+                {
+                    Console.WriteLine($"Поздравляем! Вы угадали число {secretNumber} за {attempts} попыток.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число.");
+                guess = -1; // Чтобы цикл продолжался
+            }
+        }
+
+        Console.WriteLine("Спасибо за игру!");
+    }
+}
