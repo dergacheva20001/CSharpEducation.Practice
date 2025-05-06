d87ae5631832866021283436f2abb6aeee571a20
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-06 13:30:38 +0400

    Добавила задание 2D

---
+++ CSharpEducation.Practice/Practice2.Task2d/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Введите температуру (С°): ");
+string temperatura = Console.ReadLine();
+
+if (double.TryParse(temperatura, out double gradusiC))
+{
+    double gradusiF = (gradusiC*9/5) + 32;
+            
+    Console.WriteLine($"{gradusiF}(F°)");
+}
