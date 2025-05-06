f24ab0562cd412b88aca6d6be2cc7f7c80d23de6
dergacheva20001 <dergacheva20001@mail.ru>
2025-05-06 13:36:44 +0400

    Добавила задание 17

---
+++ CSharpEducation.Practice/Practice2.Task17/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Swap(ref int a, ref int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    static void Main(string[] args)
+    {
+        int num1 = 5;
+        int num2 = 10;
+
+        Console.WriteLine("До обмена:");
+        Console.WriteLine("num1 = " + num1);
+        Console.WriteLine("num2 = " + num2);
+
+     
+        Swap(ref num1, ref num2);
+
+     
+        Console.WriteLine("После обмена:");
+        Console.WriteLine("num1 = " + num1);
+        Console.WriteLine("num2 = " + num2);
+    }
+}
